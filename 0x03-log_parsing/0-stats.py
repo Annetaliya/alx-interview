996a42685d5b7c0389807701e052688974b13678
Annetaliya <annetaliya@gmail.com>
2023-07-18 11:42:50 +0300

    log metrics calculator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
-''' 
-    python function that calculates log metrics
+''' Write a script that reads stdin line by line and computes metrics
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" 
+    <status code> <file size>
+    After every 10 lines and/or a keyboard interruption 
+    (CTRL + C), print these statistics from the beginning
 '''
+
+
 import sys
-import signal
 
-def print_statistics(total_size, status_counts):
-    '''
-        function that prints the statistics
-    '''
-    print(f"Total file size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
+'''store the count in a dictionary'''
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-def main():
-    total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-    def signal_handler(sig, frame):
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
-    signal.signal(signal.SIGINT, signal_handler)
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            parts = line.split()
-            if len(parts) != 10:
-                continue
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-            try:
-                file_size = int(parts[9])
-                status_code = int(parts[7])
-            except ValueError:
-                continue
-
+            # update total size
             total_size += file_size
-            if status_code in status_counts:
-                status_counts[status_code] += 1
 
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_counts)
+            # update count of lines
+            count += 1
 
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
-if __name__ == "__main__":
-    main()
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
