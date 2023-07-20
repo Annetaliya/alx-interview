fb97db0d3a8920e7228f57031f6e6a4b0e29ef97
Annetaliya <annetaliya@gmail.com>
2023-07-20 22:21:34 +0300

    metric calculator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' Write a script that reads stdin line by line and computes metrics
-    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" 
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
     <status code> <file size>
-    After every 10 lines and/or a keyboard interruption 
+    After every 10 lines and/or a keyboard interruption
     (CTRL + C), print these statistics from the beginning
 '''
 
